2a9ae54b5c2801943e82ffd8239abff9a2e48d71
Nikolay Marinov <nik.marinov09@gmail.com>
2025-11-01 22:50:46 +0200

    Exercise 7 initial commit

---
+++ Algorithms_introduction/zad7.py
@@ -0,0 +1,12 @@
+def selection_sort(nums: list[int]) -> None:
+    for i in range(len(nums)):
+        for j in range(i, len(nums)):
+            if nums[i] > nums[j]:
+                nums[i], nums[j] = nums[j], nums[i]
+
+
+numbers: list[int] = list(map(int, input().split()))
+
+selection_sort(numbers)
+
+print(" ".join(map(str, numbers)))
